663a0764bd105c372bf14b6fb12e01c9a7a94181
annk1995 <kabuikariuki052@gmail.com>
2021-03-09 02:53:02 -0800

    this will be my new project

--- practice.js
+++ practice.js
@@ -4,5 +4,4 @@ let employeessal=BS+finances+expenses+transport
 return employeessal
 }
 console.log(employeessal(600,1400,3200))
-console.log(employeessal(600,1400,3200))
-console.log(employeessal(600,1400,3200))
+
